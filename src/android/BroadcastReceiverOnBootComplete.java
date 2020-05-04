9ad92e5783727ec24397914cd5fcc603e4ae80d3
WMC <th@wmc-card.no>
2020-05-04 11:29:56 +0200

    Update BroadcastReceiverOnBootComplete.java

--- src/android/BroadcastReceiverOnBootComplete.java
+++ src/android/BroadcastReceiverOnBootComplete.java
@@ -3,12 +3,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.widget.Toast;
 
 public class BroadcastReceiverOnBootComplete extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
+            Context context=this.cordova.getActivity().getApplicationContext();
+            Toast.makeText(context,"On boot wmc",Toast.LENGTH_SHORT).show();
             Intent serviceIntent = new Intent(context, AndroidServiceStartOnBoot.class);
             context.startService(serviceIntent);
         }

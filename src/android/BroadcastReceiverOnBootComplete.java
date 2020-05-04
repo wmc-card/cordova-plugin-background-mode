1400b902879255171af61ded1ee7072911a57f7c
WMC <th@wmc-card.no>
2020-05-04 11:55:34 +0200

    Update BroadcastReceiverOnBootComplete.java

--- src/android/BroadcastReceiverOnBootComplete.java
+++ src/android/BroadcastReceiverOnBootComplete.java
@@ -10,7 +10,7 @@ public class BroadcastReceiverOnBootComplete extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
-            Intent serviceIntent = new Intent(context, AndroidServiceStartOnBoot.class);
+            Intent serviceIntent = new Intent(context, ForegroundService.class);
             context.startService(serviceIntent);
         }
     }

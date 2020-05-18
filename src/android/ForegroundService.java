1aceb9bc55aff54abe7057ccaaba65037a55cf02
WMC <th@wmc-card.no>
2020-05-18 10:17:00 +0200

    Update ForegroundService.java

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -112,7 +112,7 @@ public class ForegroundService extends Service {
      */
     @Override
     public int onStartCommand (Intent intent, int flags, int startId) {
-        return START_STICKY;
+        return START_NOT_STICKY;
     }
 
     /**
